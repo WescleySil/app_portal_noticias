79448211f40c351b15a31a6c9cbd43c37558c995
WescleySil <wescley.silva.cas@gmail.com>
2022-11-26 11:04:07 -0300

    first commit

---
+++ database/factories/NoticiaFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Noticia>
+ */
+class NoticiaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'titulo' => fake()->text(rand(10,40)),
+            'noticia' => fake()->text(rand(200,400))
+        ];
+    }
+}
